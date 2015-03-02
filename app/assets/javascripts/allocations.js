a96b9d81aaac10e49b786e485baca77f05e6a61b
jamespjbennett <jamespjbennett@hotmail.com>
2015-03-02 22:45:21 +0000

    basic ajax request completed to create new allocation

---
+++ app/assets/javascripts/allocations.js
@@ -0,0 +1,14 @@
+function createAllocation(form){
+
+  $percentage = form.find('input[type="text"]').val()
+  $user = form.find('input[class="user-name"]').val()
+  $invoice = $('.invoice-name').val()
+    $.ajax({
+      url: "/allocations",
+      method: 'POST',
+      dataType: 'json',
+      data: {allocation:{user_id:$user, invoice_id:$invoice, percentage:$percentage}}
+    }).done(function(data){
+      console.log(data)    
+    })
+}
