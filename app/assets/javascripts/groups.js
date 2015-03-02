bdacdecdfe44c7164ac7010b6418001455d39f33
Mary Walz <mwalz@pshift.com>
2015-03-02 16:21:08 +0000

    add group controller and index page

---
+++ app/assets/javascripts/groups.js
@@ -0,0 +1,7 @@
+// $document.ready
+// create a function that creates a gruop
+// get the value of the text box -- group name
+// make an ajax request to url /groups POST 
+// .done append somethign to say a group is created
+
+// could have a link to all the users that adds them to the group
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,18 @@
 class GroupsController < ApplicationController
 
   def index
-  
+    @groups = Group.all
+    @users = User.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.create
+  end
+  
+
 
 end
