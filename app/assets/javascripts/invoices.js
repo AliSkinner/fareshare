83d3274518987a2e1de452e5d1fc67cc06e61fb7
jamespjbennett <jamespjbennett@hotmail.com>
2015-03-03 11:20:48 +0000

    built the initial createinvoice ajax method. Will now start to test

--- app/assets/javascripts/invoices.js
+++ app/assets/javascripts/invoices.js
@@ -6,4 +6,12 @@ function createinvoice() {
   $due_date_day = $('#invoice_due_date_3i').val()
   $due_date_month = $('#invoice_due_date_2i').val()
   $due_date_year = $('#invoice_due_date_1i').val()
+    $ajax({
+      url: "/invoices",
+      method: "POST",
+      datatype: 'json',
+      data: {invoice:{name:$name, description:$description, group_id:$group, amount:$amount,due_date: $due_date_day + '/' + $due_date_month + '/' + due_date_year}}
+    }).done(function(data){
+      console.log('hello')
+    })
 }
