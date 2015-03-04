258d95fb724471dbe2d37e519e0c4a6e9fb0e697
jamespjbennett <jamespjbennett@hotmail.com>
2015-03-04 13:59:12 +0000

    allocation loop not working in the show page. Logic needs tweaking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @allocations = User.allocations
+    @allocations = @user.allocations
   end
 
   def new
