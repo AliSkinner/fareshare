ce1ab257c70a1071dea3b120ac35711023197e6e
jamespjbennett <jamespjbennett@hotmail.com>
2015-03-04 19:16:05 +0000

    user page balance now adding up only from invoices that arent paid. Need to change the invoice display so it only displays if the invoice isnt paid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @allocations = @user.allocations
-    each_user_share = @allocations.map {|allocation| allocation.share}
+    unpaid_shares = @allocations.select {|allocation| allocation.paid != true }
+    each_user_share = unpaid_shares.map {|allocation| allocation.share}
+    binding.pry
     @total_share_amount = each_user_share.reduce(:+)
   end
 
