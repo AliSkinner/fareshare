f24a94b3212a738a1652973c9447e543c0b60d7d
Ali Skinner <awskinner26@gmail.com>
2015-03-04 11:11:51 +0000

    some changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,18 @@ class UserMailer < ApplicationMailer
   default :from => "donotreply@fareshare.com"
 
   def registration_confirmation(user)
-      mail(:to => user.email, :subject => "Thanks For Registering!")
+      mail :to => user.email,
+           :subject => "Thanks For Registering!",
+           :from => "donotreply@fareshare.com"
   end
   
+end
+
+
+class YourMailer < ActionMailer::Base
+  def email_name
+    mail :subject => "Mandrill rides the Rails!",
+         :to      => "recipient@example.com",
+         :from    => "you@yourdomain.com"
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,20 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = {host: "localhost:3000"}
+  Fareshare::Application.configure do
+    config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => "MANDRILL_USERNAME",
+      :password  => "MANDRILL_PASSWORD", # SMTP password is any valid API key
+      :authentication => 'login', # Mandrill supports 'plain' or 'login'
+      :domain => 'http://localhost:3000/', # your domain to identify your server when connecting
+    }
+
+    # …
+  end
+
   
 
   # Print deprecation notices to the Rails logger.
