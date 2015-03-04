4f4e2f78000a3703c604a2149533cf0cacb6b78d
Ali Skinner <awskinner26@gmail.com>
2015-03-04 11:21:44 +0000

    Revert "some changes"
    This reverts commit f24a94b3212a738a1652973c9447e543c0b60d7d.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,18 +2,7 @@ class UserMailer < ApplicationMailer
   default :from => "donotreply@fareshare.com"
 
   def registration_confirmation(user)
-      mail :to => user.email,
-           :subject => "Thanks For Registering!",
-           :from => "donotreply@fareshare.com"
+      mail(:to => user.email, :subject => "Thanks For Registering!")
   end
   
-end
-
-
-class YourMailer < ActionMailer::Base
-  def email_name
-    mail :subject => "Mandrill rides the Rails!",
-         :to      => "recipient@example.com",
-         :from    => "you@yourdomain.com"
-  end
 end
