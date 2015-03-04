67d2f61794ce7f3196f87ce5568e596c27155328
Ali Skinner <awskinner26@gmail.com>
2015-03-04 11:33:35 +0000

    minor change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,10 +10,3 @@ class UserMailer < ApplicationMailer
 end
 
 
-class YourMailer < ActionMailer::Base
-  def email_name
-    mail :subject => "Mandrill rides the Rails!",
-         :to      => "recipient@example.com",
-         :from    => "you@yourdomain.com"
-  end
-end
