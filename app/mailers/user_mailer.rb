1f6e8912f1c02c31b83f855c2aa89af6ad13e86d
Ali Skinner <awskinner26@gmail.com>
2015-03-04 14:16:21 +0000

    removed pry points

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,10 @@ class UserMailer < ApplicationMailer
   default from: "donotreply@fareshare.com"
 
   def registration_confirmation(user)
-    # binding.pry
     @user = user
     mail(to: @user.email, subject: "Thanks For Registering!")
   end
-  
+
 end
 
 
